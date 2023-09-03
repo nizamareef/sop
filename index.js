2237e7149045aa12f4d5ae7c611aacd0c96ab6b6
areef <nizamareef@gmail.com>
2023-09-03 17:45:35 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 const express=require('express')
 const cors = require('cors');
 const dotenv=require('dotenv').config(); 
-const port=process.env.PORT
+const port=process.env.PORT || 5000
 
 const app = express();
 app.use(express.json());
