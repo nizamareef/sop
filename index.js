97aca8ac6e3c40024b324ff4f7d228f1a20059ee
areef <nizamareef@gmail.com>
2023-09-05 01:51:35 +0530

    changes

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(cors());
 
 
 mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
