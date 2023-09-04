05b44b7d5f9b3dbd6d8a91399736e032e23060c7
areef <nizamareef@gmail.com>
2023-09-04 19:40:05 +0530

    changes

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const port =  process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
-console.log(dotenv)
+// console.log(dotenv)
+  
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
+mongoose.connect(process.env.MONGODB_URI, {
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
+   
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
 
 app.use('/api', require("./Routes/form"));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
